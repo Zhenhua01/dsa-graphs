fd7bf0c49e97a9efe067a0feb0e070b5dd755203
Zhenhua <zhenhua1109@yahoo.com>
2022-09-22 11:34:26 -0700

    add remove vertex and edge

--- graph.js
+++ graph.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /** Node class for graph. */
 
 class Node {
@@ -16,16 +18,26 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    vertexArray.forEach(vertex => this.nodes.add(vertex));
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
@@ -44,4 +56,4 @@ class Graph {
   distanceOfShortestPath(start, end) { }
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
